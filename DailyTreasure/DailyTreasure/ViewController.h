981634fd3c395a9f04aa1af99d2651694f0c6a02
Yz <Yz@HumenBox.local>
2016-03-27 19:16:47 +0800

    Create the source file.

---
+++ DailyTreasure/DailyTreasure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailyTreasure
+//
+//  Created by 初号机 on 16/3/25.
+//  Copyright © 2016年 Yuz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DailyTreasure/Pods/Target Support Files/Pods-DailyTreasure/Pods-DailyTreasure-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DailyTreasure : NSObject
+@end
+@implementation PodsDummy_Pods_DailyTreasure
+@end
